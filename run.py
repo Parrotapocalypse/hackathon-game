23566468dca7444c96018d17d373254a2d2e404a
Oliver FitzPatrick <oliverfitzpatrick00@gmail.com>
2024-02-09 19:55:24 -0500

    First steps

--- run.py
+++ run.py
@@ -1,7 +1,14 @@
-import pygame
+import pygame as pg
 
 def main():
-    pass
+    pg.init()
+    screen = pg.display.set_mode((1280, 480), pg.SCALED)
+    pg.display.set_caption("Biblically Accurate Angel Simulator")
+    pg.mouse.set_visible(False)
+    background = pg.Surface(screen.get_size())
+
+    pg.draw.circle(background, pg.Color(100,0,100), (0,0), 5)
+    input()
 
 if __name__ == '__main__':
     main()
