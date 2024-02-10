af2d02446f8ad0fca53a9f5f3f359f65d181adaa
Oliver FitzPatrick <oliverfitzpatrick00@gmail.com>
2024-02-10 10:24:29 -0500

    Submission

--- obstacles.py
+++ obstacles.py
@@ -6,23 +6,34 @@ import random
 class Obstacle(pg.sprite.Sprite):
     def __init__(self, screen:pg.surface.Surface, flipped=False) -> None:
         super().__init__()
+        self.collided = False
+        self.flipped = flipped
+        self.fast = flipped
         rotation = 0
         if flipped: rotation = 180
-        self.image, self.rect = assets.load_image('images/fire.png', rotation)
+        self.image, self.rect = assets.load_image('images/firewall_small.png', rotation, 0.25)
         # needed for transparency reasons
         colorkey = self.image.get_at((0, 0))
         self.image.set_colorkey(colorkey)
 
         # should be on right side of screen to start
         topright = screen.get_rect().topright
-        self.rect.centerx = topright[0]
+        scaler = random.randrange(10,25) / 10
+        self.image = pg.transform.scale(self.image, (self.rect.width/2,self.rect.height*scaler))
+        self.rect = self.image.get_rect()
         if flipped:
-            self.rect.centery = topright[1] + random.randrange(0,screen.get_rect().centery)
-            self.image = pg.transform.scale(self.image, (self.rect.width, self.rect.centery))
+            self.rect.topright = topright
         else:
-            self.rect.centery = screen.get_rect().bottomright[1] + random.randrange(0,screen.get_rect().centery)
-            self.image = pg.transform.scale(self.image, (self.rect.width, abs(screen.get_rect().bottom - self.rect.centery)))
+            self.rect.bottomright = screen.get_rect().bottomright
     def update(self, *args: Any, **kwargs: Any):
         speed = kwargs.get('speed', 1)
+        if self.fast: speed *= 1.5
         self.rect = self.rect.move(-speed,0)
         if self.rect.topright[0] < kwargs.get('screen').get_rect().topleft[0]: self.kill()
+        
+        angel_rect = kwargs.get('collide')
+        if self.rect.colliderect(angel_rect):
+            if self.flipped:
+                self.collided = angel_rect.top < (self.rect.bottom + self.rect.centery)/2 and angel_rect.centerx >= self.rect.left
+            else:
+                self.collided = angel_rect.bottom > (self.rect.top + self.rect.centery)/2 and angel_rect.centerx >= self.rect.left
--- run.py
+++ run.py
@@ -5,16 +5,16 @@ import character
 import math
 
 # constants
-MAX_OBSTACLES = 2
+MAX_OBSTACLES = 3
 SPEED = 2
 SQRT = math.sqrt(SPEED)
-ACCELERATION = 1e-3
+ACCELERATION = 1.5e-3
 
 def main():
     # set up main game stuff
     pg.init()
     clock = pg.time.Clock()
-    screen = pg.display.set_mode((1280, 480), pg.FULLSCREEN)
+    screen = pg.display.set_mode((1920, 1080), pg.FULLSCREEN)
     pg.display.set_caption("Biblically Accurate Angel Simulator")
     pg.mouse.set_visible(True)
     # set up background and display it
@@ -27,8 +27,9 @@ def main():
     angel = character.Angel(screen)
     obstacle1 = obstacles.Obstacle(screen)
     obstacle2 = obstacles.Obstacle(screen, True)
-    allsprites = pg.sprite.RenderPlain((angel, obstacle1, obstacle2))
-    spriteobstacles = pg.sprite.RenderPlain((obstacle1, obstacle2))
+    obstacle3 = obstacles.Obstacle(screen, rand_bool())
+    allsprites = pg.sprite.RenderPlain((angel, obstacle1, obstacle2, obstacle3))
+    spriteobstacles = pg.sprite.RenderPlain((obstacle1, obstacle2, obstacle3))
     going = True # infinite game loop
     howfast = 0
     score = 0
@@ -38,11 +39,9 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 going = False
-                break
             elif event.type == pg.KEYDOWN:
                 if event.key == pg.K_ESCAPE:
                     going = False
-                    break
         pressed = pg.key.get_pressed()
         def delta(x,y):
             angel.rect.x += x
@@ -57,15 +56,17 @@ def main():
             else: angel.rect.y += SPEED
         elif pressed[pg.K_LEFT]: angel.rect.x -= SPEED
         elif pressed[pg.K_RIGHT]: angel.rect.x += SPEED
-        try:
-            allsprites.update(speed=1+howfast, screen=screen, collide=spriteobstacles)
-        except:
-            break
+        allsprites.update(speed=1+howfast, screen=screen, collide=angel.rect)
+        for obst in spriteobstacles:
+            if obst.collided: going = False
         # sprites might have been removed from the group
         if len(spriteobstacles) < MAX_OBSTACLES:
+            score += 1
             sprite = obstacles.Obstacle(screen, rand_bool())
             allsprites.add(sprite)
             spriteobstacles.add(sprite)
+            if sprite.flipped: sprite.rect.topright = screen.get_rect().topright
+            else: sprite.rect.bottomright = screen.get_rect().bottomright
         image.fill((200,100,50))
         # do the actual displaying of stuff
         allsprites.draw(image)
@@ -73,13 +74,14 @@ def main():
         screen.blit(background, (0,0))
         pg.display.flip()
         howfast += ACCELERATION
-        score += 1/60
-    if going:
-        font = pg.font.Font(None, 64)
-        text = font.render(f"Your Score: {score}", True, (10, 10, 10))
-        textpos = text.get_rect(centerx=background.get_width() / 2, y=10)
-        background.blit(text, textpos)
-        pg.display.flip()
+    font = pg.font.Font(None, 64)
+    text = font.render(f"Your Score: {score}. Press any key to exit", True, (10, 10, 10))
+    textpos = text.get_rect(centerx=background.get_width() / 2, y=10)
+    background.blit(text, textpos)
+    screen.blit(background, (0,0))
+    pg.display.flip()
+    e = pg.event.wait()
+    while e.type != pg.KEYDOWN: e = pg.event.wait()
     pg.quit()
 
 def rand_bool():
